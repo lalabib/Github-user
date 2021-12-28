fe8af1db4812f9798613ceea9c1079e93f6de4c3
lalabib <labibla21@gmail.com>
2021-12-28 22:43:01 +0700

    fix viewbinding on adapter

--- app/src/main/java/com/project/githubuser/adapter/ListUserAdapter.kt
+++ app/src/main/java/com/project/githubuser/adapter/ListUserAdapter.kt
@@ -2,13 +2,11 @@ package com.project.githubuser.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.project.githubuser.R
+import com.project.githubuser.databinding.UserListBinding
 import com.project.githubuser.model.User
-import kotlinx.android.synthetic.main.user_list.view.*
 
 class ListUserAdapter(private val listUser: ArrayList<User>):
     RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
@@ -26,10 +24,8 @@ class ListUserAdapter(private val listUser: ArrayList<User>):
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ListViewHolder {
-        return (ListViewHolder(
-            LayoutInflater.from(viewGroup.context).inflate(R.layout.user_list, viewGroup, false)
-        ))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        return ListViewHolder(UserListBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
@@ -40,12 +36,14 @@ class ListUserAdapter(private val listUser: ArrayList<User>):
     }
 
     override fun getItemCount(): Int = listUser.size
-    class ListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    class ListViewHolder(private val binding: UserListBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(user: User) {
-            with(itemView) {
-                Glide.with(itemView.context).load(user.avatar_url).circleCrop().into(img_item_photo)
-                tv_item_name.text = user.login
-            }
+            Glide.with(itemView.context)
+                .load(user.avatar_url)
+                .circleCrop()
+                .into(binding.imgItemPhoto)
+            binding.tvItemName.text = user.login
         }
     }
 
