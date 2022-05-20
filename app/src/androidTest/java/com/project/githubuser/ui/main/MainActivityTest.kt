dbd2838d3593a3d808b367e868ddac6c94fb5a3a
lalabib <labibla21@gmail.com>
2022-05-20 23:42:51 +0700

    Github-user: add ui testing

--- app/src/androidTest/java/com/project/githubuser/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/project/githubuser/ui/main/MainActivityTest.kt
@@ -1,8 +1,24 @@
 package com.project.githubuser.ui.main
 
-import org.junit.Assert.*
+import android.view.KeyEvent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.project.githubuser.R
+import org.junit.Before
+import org.junit.Test
 
 class MainActivityTest {
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
 
-
+    @Test
+    fun searchView() {
+        onView(withId(R.id.search)).perform(click())
+        onView(withId(R.id.search_src_text)).perform(typeText("labib"))
+        onView(withId(R.id.search)).perform(pressKey(KeyEvent.KEYCODE_ENTER))
+    }
 }
